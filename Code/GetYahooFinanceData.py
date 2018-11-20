a487e9123d9b103fbc6c250ce53957702325f02c
Sam <sailic97@gmail.com>
2018-11-20 09:40:22 -0600

    IBM Stock Data

--- Code/GetYahooFinanceData.py
+++ Code/GetYahooFinanceData.py
@@ -12,10 +12,10 @@ from pandas_datareader import data as pdr
 import pandas as pd
 
 ### Set Parameters
-fileName = "UNG"
+fileName = "IBM"
 stock = fileName  # "IJS"
-startDate = "2014-11-11"  # yyyy-mm-dd
-endDate = "2016-09-01"
+startDate = "2002-01-01"  # yyyy-mm-dd
+endDate = "2018-10-31"
 
     # start_date = datetime.datetime(2014, 11, 11)
     # end_date = datetime.datetime(2016, 9, 1)
@@ -24,5 +24,6 @@ endDate = "2016-09-01"
 df = pdr.get_data_yahoo(stock, startDate, endDate)
 
 ### Write to CSV
-filePath  = r"/Users/YoungFreeesh/QSTrader Data/" + fileName + ".csv"  # file path 
+#filePath  = r"/Users/YoungFreeesh/QSTrader Data/" + fileName + ".csv"  # file path 
+filePath  = r"/Users/YoungFreeesh/Documents/GitHub/Stat479_Project/Data/" + fileName + ".csv"  # file path 
 df.to_csv(filePath)
